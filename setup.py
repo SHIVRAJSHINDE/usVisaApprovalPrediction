de9d16f46dea8d5f5110fed10da6c5d072f7d9b9
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-04-02 17:30:18 +0530

    MongoDb utils exp Logs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="ShivrajShinde",
+    author_email="shindeshivraaj@gmail.com",
+    packages=find_packages()
+)
